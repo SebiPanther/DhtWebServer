96469059cfba0bcf3860e24e98242888aa51e4e4
SebiPanther <SebastianUthoff@zoowatching.de>
2016-01-23 15:03:51 +0100

    csv file rolling log

--- DhtWebServer.ino
+++ DhtWebServer.ino
@@ -1,9 +1,15 @@
-#include <ESP8266mDNS.h>
+#include <WiFiManager.h>
 #include <NTPClient.h>
+#include <DHT.h>
+#include <ESP8266mDNS.h>
+#include <ESP8266WebServer.h>
+#include <WiFiUdp.h>
+#include <WiFiServer.h>
+#include <WiFiClientSecure.h>
+#include <WiFiClient.h>
+#include <ESP8266WiFiMulti.h>
 #include <ESP8266WiFi.h>
 #include "FS.h"
-#include <DHT.h>
-#include <WiFiManager.h>
 
 
 #define DHTPIN 2
@@ -18,95 +24,160 @@ NTPClient timeClient(ntpServer, 3600, 60000);
 WiFiManager wifiManager;
 ESP8266WebServer server(80);
 
+unsigned long curDataPointPos = 0;
 unsigned long lastTimeMeasured = 0;
 float lastHumidity = NAN;
 float lastTemperature = NAN;
 
 void handleRoot() {
-    File valuesFile = SPIFFS.open(fileName, "r");
-    if (!valuesFile) {
-      server.send(404, "text/plain", "values.csv not Found or failure by open it.");
-    }
-    else
-    {
-      server.streamFile(valuesFile, "text/plain");
-    }
-    valuesFile.close();
+  File valuesFile = SPIFFS.open(fileName, "r");
+  if (!valuesFile) {
+    server.send(404, "text/plain", "values.csv not Found or failure by open it.");
+  }
+  else
+  {
+    server.streamFile(valuesFile, "text/plain");
+  }
+  valuesFile.close();
 }
 
-void handleNotFound(){
+void handleNotFound() {
   String message = "File Not Found\n\n";
   message += "URI: ";
   message += server.uri();
   message += "\nMethod: ";
-  message += (server.method() == HTTP_GET)?"GET":"POST";
+  message += (server.method() == HTTP_GET) ? "GET" : "POST";
   message += "\nArguments: ";
   message += server.args();
   message += "\n";
-  for (uint8_t i=0; i<server.args(); i++){
+  for (uint8_t i = 0; i<server.args(); i++) {
     message += " " + server.argName(i) + ": " + server.arg(i) + "\n";
   }
   server.send(404, "text/plain", message);
 }
 
-void writeFile(unsigned long longTime, float h, float t)
+unsigned long findlastPosition()
 {
-      Serial.print(longTime);
-      Serial.print(";");
-      Serial.print(h);
-      Serial.print(";");
-      Serial.println(t);
-      
-      File valuesFile = SPIFFS.open(fileName, "a");
-      if (!valuesFile) {
-          Serial.println("file open failed");
-      }
-      else
+  unsigned long maxLine = 0;
+  File valuesFile = SPIFFS.open(fileName, "r");
+  if (!valuesFile) {
+    Serial.println("file open failed - maxline = 0");
+  }
+  else
+  {
+    valuesFile.seek(0, SeekSet);
+    unsigned long maxTicks = 0;
+    unsigned long lineCount = 0;
+    while (valuesFile.available())
+    {
+      String curValues = valuesFile.readStringUntil('\n');
+      char buf[11];
+      Serial.println(curValues);
+      curValues.substring(0, 10).toCharArray(buf, 11);
+      unsigned long longTime = strtoul(buf, '\0', 10);
+      if (maxTicks <= longTime)
       {
-        valuesFile.print(longTime);
-        valuesFile.print(";");
-        valuesFile.print(h);
-        valuesFile.print(";");
-        valuesFile.println(t);
+        maxLine = lineCount;
+        maxTicks = longTime;
       }
-      valuesFile.close();
+      lineCount++;
+    }
+    maxLine++;
+  }
+  valuesFile.close();
+  return maxLine;
 }
 
-void setup() {
-    Serial.begin(115200);
-    wifiManager.autoConnect();
-    
-    SPIFFS.begin();
-    SPIFFS.remove(fileName);
+void writeFile(unsigned long longTime, float h, float t)
+{
+  if (!SPIFFS.exists(fileName))
+  {
+    File valuesFile = SPIFFS.open(fileName, "w");
+    valuesFile.close();
+  }
+  File valuesFile = SPIFFS.open(fileName, "r+");
+  if (!valuesFile) {
+    Serial.println("file open failed");
+  }
+  else
+  {
+    valuesFile.seek(0, SeekSet);
+    if (curDataPointPos < maxDataPoints)
+    {
+      unsigned long lineCount = 0;
+      while (valuesFile.available())
+      {
+        String curValues = valuesFile.readStringUntil('\n');
+        if (lineCount >= curDataPointPos)
+        {
+          break;
+        }
+        lineCount++;
+      }
+    }
+    else
+    {
+      curDataPointPos = 0;
+    }
 
-    MDNS.begin(mDnsName);
-    MDNS.addService("http", "tcp", 80);
-    Serial.print("MDNS Name: ");
-    Serial.println(mDnsName);
-    
-    server.on("/", handleRoot);
-    server.onNotFound(handleNotFound);
-    server.begin();
-    Serial.print("Server: http://");
-    Serial.print(WiFi.localIP());
-    Serial.println("/");
-    
-    lastTimeMeasured = millis();
+    Serial.print("Pos: ");
+    Serial.print(curDataPointPos);
+    Serial.print("(");
+    Serial.print(valuesFile.position());
+    Serial.print("): ");
+
+    valuesFile.print(longTime);
+    valuesFile.print(";");
+    valuesFile.print(h);
+    valuesFile.print(";");
+    valuesFile.println(t);
+    curDataPointPos++;
+
+    Serial.print(longTime);
+    Serial.print(";");
+    Serial.print(h);
+    Serial.print(";");
+    Serial.println(t);
+  }
+  valuesFile.close();
+}
+
+
+void setup() {
+  Serial.begin(115200);
+  wifiManager.autoConnect();
+
+  SPIFFS.begin();
+
+  MDNS.begin(mDnsName);
+  MDNS.addService("http", "tcp", 80);
+  Serial.print("MDNS Name: ");
+  Serial.println(mDnsName);
+
+  server.on("/", handleRoot);
+  server.onNotFound(handleNotFound);
+  server.begin();
+  Serial.print("Server: http://");
+  Serial.print(WiFi.localIP());
+  Serial.println("/");
+
+  curDataPointPos = findlastPosition();
+  lastTimeMeasured = millis();
 }
 
 void loop() {
   server.handleClient();
-  if((lastTimeMeasured + 5000) < millis())
+  if ((lastTimeMeasured + 5000) < millis())
   {
     timeClient.update();
-    
+
     float h = dht.readHumidity();
     float t = dht.readTemperature();
 
-    if(!isnan(h) && !isnan(t) && (h != lastHumidity || t != lastTemperature))
-    {      
+    if (!isnan(h) && !isnan(t) && (h != lastHumidity || t != lastTemperature))
+    {
       writeFile(timeClient.getRawTime(), h, t);
-            
+
       lastHumidity = h;
       lastTemperature = t;
       lastTimeMeasured = millis();
