497da0af8c0a5dbf89ab97a6b7e096922ed30a8d
SebiPanther <SebastianUthoff@zoowatching.de>
2016-01-24 02:42:20 +0100

    Provide Datapoints as json and csv. Both ordered by there Messurement Date

--- DhtWebServer.ino
+++ DhtWebServer.ino
@@ -14,7 +14,7 @@
 
 #define DHTPIN 2
 #define DHTTYPE DHT11   // DHT 11
-int maxDataPoints = 100;
+int maxDataPoints = 500;
 char ntpServer[] = "europe.pool.ntp.org";
 char fileName[] = "/values.csv";
 char mDnsName[] = "espDht";
@@ -29,31 +29,159 @@ unsigned long lastTimeMeasured = 0;
 float lastHumidity = NAN;
 float lastTemperature = NAN;
 
-void handleRoot() {
+void handleCsv() {
   File valuesFile = SPIFFS.open(fileName, "r");
   if (!valuesFile) {
-    server.send(404, "text/plain", "values.csv not Found or failure by open it.");
+    server.send(404, "text/plain", "Csv not Found or failure by open it.");
   }
   else
   {
-    server.streamFile(valuesFile, "text/plain");
+    WiFiClient client = server.client();
+    client.println("HTTP/1.1 200 OK");
+    client.println("Content-Type: text/plain");
+    client.print("Content-Length: ");
+    client.println(valuesFile.size());
+    client.println();
+    unsigned long lineCount = 0;
+    while (valuesFile.available())
+    {
+      String curValues = valuesFile.readStringUntil('\n');
+      if (lineCount > curDataPointPos)
+      {
+        client.print(curValues);
+      }
+      lineCount++;
+    }
+    valuesFile.seek(0, SeekSet);
+    lineCount = 0;
+    while (valuesFile.available())
+    {
+      String curValues = valuesFile.readStringUntil('\n');
+      if (lineCount <= curDataPointPos)
+      {
+        client.print(curValues);
+      }
+      else
+      {
+        break;
+      }
+      lineCount++;
+    }
+  }
+  valuesFile.close();
+}
+
+void writeJsonObj(WiFiClient client, String curValues) {
+  client.print("{\"Time\":");
+  client.print(curValues.substring(0, 10));
+  client.print(",");
+  client.print("\"Humidity\":");
+  client.print(curValues.substring(11, 16));
+  client.print(",");
+  client.print("\"Temperature\":");
+  client.print(curValues.substring(17, 22));
+  client.print("}");
+}
+
+void handleJson() {
+  File valuesFile = SPIFFS.open(fileName, "r");
+  if (!valuesFile) {
+    server.send(404, "text/plain", "Json not Found or failure by open it.");
+  }
+  else
+  {
+    WiFiClient client = server.client();
+    client.println("HTTP/1.1 200 OK");
+    client.println("Content-Type: application/json");
+    client.println();
+    client.println("[");
+    unsigned long lineCount = 0;
+    while (valuesFile.available())
+    {
+      String curValues = valuesFile.readStringUntil('\n');
+      if (lineCount > curDataPointPos)
+      {
+        writeJsonObj(client, curValues);
+        if (!valuesFile.available() && curDataPointPos >= maxDataPoints)
+        {
+          client.println("");
+        }
+        else
+        {
+          client.println(",");
+        }
+      }
+      lineCount++;
+    }
+    valuesFile.seek(0, SeekSet);
+    lineCount = 0;
+    while (valuesFile.available())
+    {
+      String curValues = valuesFile.readStringUntil('\n');
+      if (lineCount <= curDataPointPos)
+      {
+        writeJsonObj(client, curValues);
+        if (lineCount < curDataPointPos)
+        {
+          client.println(",");
+        }
+        else
+        {
+          client.println("");
+        }
+      }
+      else
+      {
+        break;
+      }
+      lineCount++;
+    }
+    client.println("]");
   }
   valuesFile.close();
 }
 
 void handleNotFound() {
-  String message = "File Not Found\n\n";
-  message += "URI: ";
-  message += server.uri();
-  message += "\nMethod: ";
-  message += (server.method() == HTTP_GET) ? "GET" : "POST";
-  message += "\nArguments: ";
-  message += server.args();
-  message += "\n";
-  for (uint8_t i = 0; i<server.args(); i++) {
-    message += " " + server.argName(i) + ": " + server.arg(i) + "\n";
+  String path = server.uri();;
+  if (path == "/")
+  {
+    path = "/index.html";
   }
-  server.send(404, "text/plain", message);
+
+  String dataType = "text/plain";
+  if (path.endsWith(".htm")) dataType = "text/html";
+  else if (path.endsWith(".css")) dataType = "text/css";
+  else if (path.endsWith(".js")) dataType = "application/javascript";
+  else if (path.endsWith(".png")) dataType = "image/png";
+  else if (path.endsWith(".gif")) dataType = "image/gif";
+  else if (path.endsWith(".jpg")) dataType = "image/jpeg";
+  else if (path.endsWith(".ico")) dataType = "image/x-icon";
+  else if (path.endsWith(".xml")) dataType = "text/xml";
+  else if (path.endsWith(".pdf")) dataType = "application/pdf";
+  else if (path.endsWith(".zip")) dataType = "application/zip";
+
+  File file = SPIFFS.open(path, "r");
+  if (!file)
+  {
+    String message = "File Not Found or unable to Load.\n\n";
+    message += "URI: ";
+    message += server.uri();
+    message += "\nMethod: ";
+    message += (server.method() == HTTP_GET) ? "GET" : "POST";
+    message += "\nArguments: ";
+    message += server.args();
+    message += "\n";
+    message += "Maybe you should try /csv or /json ;)";
+    for (uint8_t i = 0; i < server.args(); i++) {
+      message += " " + server.argName(i) + ": " + server.arg(i) + "\n";
+    }
+    server.send(404, "text/plain", message);
+  }
+  else
+  {
+    server.streamFile(file, dataType);
+  }
+  file.close();
 }
 
 unsigned long findlastPosition()
@@ -149,13 +277,15 @@ void setup() {
   wifiManager.autoConnect();
 
   SPIFFS.begin();
+  //SPIFFS.remove(fileName);
 
   MDNS.begin(mDnsName);
   MDNS.addService("http", "tcp", 80);
   Serial.print("MDNS Name: ");
   Serial.println(mDnsName);
 
-  server.on("/", handleRoot);
+  server.on("/csv", handleCsv);
+  server.on("/json", handleJson);
   server.onNotFound(handleNotFound);
   server.begin();
   Serial.print("Server: http://");
